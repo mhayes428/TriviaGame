0dc3d3758f78f518ad3acfef330224f4826a19b5
Mike Hayes <fikes428@gmail.com>
2019-07-29 10:58:40 -0400

    consoleLog

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     $("#remaining-time").hide();
     $("#start").on('click', trivia.startGame);
-    $(document).on('click' , '.option', trivia.guessChecker);
+    $(document).on('click', '.option', trivia.guessChecker);
     
 })
   
