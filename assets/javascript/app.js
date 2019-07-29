7325854bcd36cd5ac90432c509f8b31c1f599cc3
Mike Hayes <fikes428@gmail.com>
2019-07-29 10:50:39 -0400

    firstJScode

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  
+    $("#remaining-time").hide();
+    $("#start").on('click', trivia.startGame);
+    $(document).on('click' , '.option', trivia.guessChecker);
+    
+  })
