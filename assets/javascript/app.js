91948ac52c5b0b67238625ae536ddce17927ffa7
Mike Hayes <fikes428@gmail.com>
2019-07-29 10:56:32 -0400

    addVariablesQuestions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,4 +4,25 @@ $(document).ready(function(){
     $("#start").on('click', trivia.startGame);
     $(document).on('click' , '.option', trivia.guessChecker);
     
-  })
+})
+  
+var trivia = {
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 20,
+    timerOn: false,
+    timerId: "",
+    
+    questions: {
+        q1: "Who is ",
+        q2: "",
+        q3: "",
+        q4: "",
+        q5: "",
+        q6: "",
+        q7: "",
+
+    }
+}
