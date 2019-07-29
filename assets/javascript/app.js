8c8c69e0c486ac4ae990a94543fdce41f6cb44f9
Mike Hayes <fikes428@gmail.com>
2019-07-29 11:15:18 -0400

    questonsAnswersOptionsAdd

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,13 +19,19 @@ var trivia = {
         q1: "Who refers to The Ring as 'My precious'?",
         q2: "What is Gollum's actual birth name?",
         q3: "Where does The Ring have to be destroyed?",
-        q4: "They are known as 'Ringwraiths, Black Riders, and 'the Nine', but what is their true title?",
+        q4: "They are known as 'Ringwraiths, Black Riders, and 'the Nine', but what is their true title?"
 
     },
     options: {
+        q1: ["Gollum", "Frodo Baggins", "Bilbo Baggins", "Samwise Gamgee"],
+        q2: ["Smeagol", "Strider", "Nazgul", "Sauron"],
+        q3: ["Mount Doom", "The Shire", "Dead Marshes", "Dale"],
+        q4: ["Nazgul", "Uruk-hai", "Wargs", "Fell beasts"]
+    },
+    answers: {
         q1: "Gollum",
         q2: "Smeagol",
         q3: "Mount Doom",
-        q4: "Nazgul",
+        q4: "Nazgul"
     }
 }
